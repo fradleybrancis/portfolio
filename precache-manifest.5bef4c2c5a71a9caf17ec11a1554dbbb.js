68a4c44df8a4dfea495c3a6ca24a2c8dcb196197
fradleybrancis <fradleybrancis@gmail.com>
2020-02-07 13:21:24 -0800

    Updates

---
+++ precache-manifest.5bef4c2c5a71a9caf17ec11a1554dbbb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "66104f32edbb1a2951ad",
+    "url": "/portfolio/static/css/main.8dbf0fca.chunk.css"
+  },
+  {
+    "revision": "66104f32edbb1a2951ad",
+    "url": "/portfolio/static/js/main.66104f32.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "a0167d38f43cfad4abc14196ed38bd67",
+    "url": "/portfolio/index.html"
+  }
+];
