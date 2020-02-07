bc52e6daeb65ea04fb399690e4e0afdfd70734c3
fradleybrancis <fradleybrancis@gmail.com>
2020-02-07 12:30:08 -0800

    initial commit

---
+++ src/resumeData.js
@@ -0,0 +1,103 @@
+let resumeData = {
+    "name": "Bradley Francis",
+    "role": "Full Stack Developer",
+    "linkedinId":"fradleybrancis",
+    "skypeid": "bradleyjfrancis92",
+    "roleDescription": " Currently living in San Francisco",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/fradleybrancis/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/fradleybrancis",
+          "className":"fa fa-github",
+        },
+      ],
+    "aboutme":"Born and raised in Southern California but I call the Bay Area home now. Fluent with the MERN (Mongo, Express, React, Node) stack and have deployed multiple applications with AWS and Heroku. Outside of developing I love to skateboard and play video games!",
+    "address":"San Francisco",
+    "email":"bradleyjfrancis92@gmail.com",
+    "education":[
+      {
+        "UniversityName":"Hack Reactor - SF",
+        "specialization":"Fullstack Developer Immersive",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2019",
+      },
+      {
+        "UniversityName":"California State University, East Bay",
+        "specialization":"Marketing / Advertising & Public Relations",
+        "MonthOfPassing":"March",
+        "YearOfPassing":"2018",
+      },
+    ],
+    "work":[
+      {
+        "CompanyName":"Market Broiler",
+        "specialization":"Waiter",
+        "MonthOfLeaving":"June",
+        "YearOfLeaving":"2018",
+      },
+      {
+        "CompanyName":"Harbor Frieght Tools",
+        "specialization":"Sales Associate",
+        "MonthOfLeaving":"March",
+        "YearOfLeaving":"2016",
+      }
+    ],
+    "skillsDescription":"Stack",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Skate Log",
+        "description":"Save Images to a Location via Google Maps API",
+        "imgurl":"images/skateLog2.png",
+        "link": "https://skate-log.herokuapp.com/",
+        "repository": "https://github.com/fradleybrancis/SkateLog",
+      },
+      {
+        "name":"Babbler",
+        "description":"Chat App Made With Node, WebSockets, and React",
+        "imgurl":"images/babbler-chat2.png",
+        "link": "https://babbler-chat.herokuapp.com/",
+        "repository": "https://github.com/fradleybrancis/babbler",
+      },
+      {
+        "name":"Abode.ly",
+        "description":"Reviews Module of Housing Rental App",  
+        "imgurl":"images/abode.ly.reviews.png",
+        "repository": "https://github.com/abodely/reviews_module",
+      },
+      {
+        "name":"Go-To-Rentals",
+        "description":"Related Products to Rental App",  
+        "imgurl":"images/stressTest.png",
+        "repository": "https://github.com/hrsf113-group-2/proxy_related_listings",       
+
+      },
+    ],
+    "testimonials":[
+      {
+        "description":"He's pretty smart I guess, Why you putting me on the spot?",
+        "name":"Matt Goldwater",
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy",
+      }
+    ]
+  }
+  
+  export default resumeData
